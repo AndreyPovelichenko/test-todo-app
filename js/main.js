9da61940b51e7a4aedc71dc9ff4302a39919c77a
Andrii Povelychenko <andrii_povelychenko@epam.com>
2015-10-06 20:54:47 +0300

    add requirejs config

---
+++ js/main.js
@@ -0,0 +1,20 @@
+requirejs.config({
+   paths: {
+       jquery: 'bower_components/jquery/dist/jquery',
+       underscore: 'bower_components/underscore/underscore',
+       backbone: 'bower_components/backbone/backbone',
+       text: 'bower_components/text/text',
+
+       templates: '../templates',
+       app: 'app'
+   }
+});
+
+requirejs([
+    'jquery',
+    'underscore',
+    'backbone',
+    'app'
+], function($, _, Backbone, App) {
+    App.init();
+});
